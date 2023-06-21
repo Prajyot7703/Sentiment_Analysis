b6951f8719ea165fd908b91da1af374bfee648c6
Prajyot7703 <86780905+Prajyot7703@users.noreply.github.com>
2023-06-22 02:37:59 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,13 +3,10 @@ import streamlit as st
 from transformers import pipeline
 sent_pipeline = pipeline("sentiment-analysis")
 
-# sent_pipeline('I love sentiment analysis!')
-# print(sent_pipeline('Booo')[0]["label"])
-
-
-
 def main():
-    st.title("Sentiment Analyzer")
+
+    # st.title("Sentiment Analyzer")
+
     html_temp = """
     <div style="background-color:tomato;padding:10px">
     <h2 style="color:white;text-align:center;">Sentiment Analyzer </h2>
@@ -17,15 +14,11 @@ def main():
     """
     st.markdown(html_temp,unsafe_allow_html=True)
     # st.markdown("# TEXT")
-    text = st.text_input("",placeholder="Type here ...")
-    # skewness = st.text_input("skewness","Type Here")
-    # curtosis = st.text_input("curtosis","Type Here")
-    # entropy = st.text_input("entropy","Type Here")
+    text = st.text_input(" ",placeholder="Type here ...")
+
     # result=""
-    
     if st.button("Predict"):
         result=sent_pipeline(text)[0]["label"]
-        # result=predict_note_authentication(variance,skewness,curtosis,entropy)
         # st.success('The output is {}'.format(result))
         st.info(f"Sentiment : {result}")
 
